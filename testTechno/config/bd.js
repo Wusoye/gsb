29feea2457a765387d9af1eb863ba46d524a8704
Wusoye <76995159+Wusoye@users.noreply.github.com>
2021-05-06 10:52:29 +0200

    Update bd.js

--- testTechno/config/bd.js
+++ testTechno/config/bd.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL -> mongodb://XXX.XXX.XX.XXX:27017
-const url = 'mongodb://localhost:27017'; 
+const url = 'mongodb://192.168.30.205:27017'; 
 
 // Database Name
 const client = new MongoClient(url);
